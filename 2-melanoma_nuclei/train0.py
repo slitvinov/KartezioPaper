05fc36a777deef6d348f28795e1a38238b91e2a2
Sergey Litvinov <slitvinov@gmail.com>
2024-09-20 21:21:14 +0200

    up

--- 2-melanoma_nuclei/train0.py
+++ 2-melanoma_nuclei/train0.py
@@ -27,6 +27,8 @@ import os
 import pandas as pd
 import random
 import time
+from abc import ABC, abstractmethod
+from typing import List
 from numena.image.drawing import (
     draw_overlay,
     fill_ellipses_as_labels,
@@ -50,8 +52,84 @@ from kartezio.preprocessing import TransformToHED, TransformToHSV
 from kartezio.stacker import MeanKartezioStackerForWatershed
 from kartezio.stacker import StackerMean
 from kartezio.utils.io import JsonSaver
-from kartezio.model.helpers import Factory, Observer, Prototype
 from kartezio.model.registry import registry
+
+def singleton(cls):
+    """
+    https://towardsdatascience.com/10-fabulous-python-decorators-ab674a732871
+    """
+    instances = {}
+
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return wrapper
+
+
+class Prototype(ABC):
+    """
+    Using Prototype Pattern to duplicate:
+    https://refactoring.guru/design-patterns/prototype
+    """
+
+    @abstractmethod
+    def clone(self):
+        pass
+
+
+class Factory:
+    """
+    Using Factory Pattern:
+    https://refactoring.guru/design-patterns/factory-method
+    """
+
+    def __init__(self, prototype):
+        self._prototype = None
+        self.set_prototype(prototype)
+
+    def set_prototype(self, prototype):
+        self._prototype = prototype
+
+    def create(self):
+        return self._prototype.clone()
+
+
+class Observer(ABC):
+    """
+    The Observer interface declares the update method, used by subjects.
+    """
+
+    @abstractmethod
+    def update(self, event):
+        """
+        Receive update from subject.
+        """
+        pass
+
+
+class Observable(ABC):
+    """
+    For the sake of simplicity, the Observable state, essential to all
+    subscribers, is stored in this variable.
+    """
+
+    def __init__(self):
+        self._observers: List[Observer] = []
+
+    def attach(self, observer: Observer) -> None:
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def clear(self) -> None:
+        self._observers = []
+
+    def notify(self, event) -> None:
+        for observer in self._observers:
+            observer.update(event)
 
 
 def register_endpoints():
