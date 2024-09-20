a2b0cf2ea1b8b5959d88237436bb63c992d95f5d
Sergey Litvinov <slitvinov@gmail.com>
2024-09-20 21:06:19 +0200

    up

--- 2-melanoma_nuclei/train0.py
+++ 2-melanoma_nuclei/train0.py
@@ -5,6 +5,12 @@ from kartezio.endpoint import (
     EndpointWatershed,
     LocalMaxWatershed,
 )
+from kartezio.model.components import (
+    GenomeReaderWriter,
+    KartezioComponent,
+    KartezioGenome,
+    KartezioNode,
+)
 from abc import ABC, abstractmethod
 from dataclasses import InitVar, dataclass, field
 from numena.io.drive import Directory
@@ -25,14 +31,13 @@ from typing import List
 import numpy as np
 from numena.io.json import Serializable
 
+from kartezio.model.types import Score, ScoreList
 from kartezio.callback import CallbackSave, CallbackVerbose
 from kartezio.callback import Event
 from kartezio.dataset import read_dataset
 from kartezio.endpoint import EndpointThreshold
 from kartezio.enums import CSV_DATASET
 from kartezio.image.bundle import BUNDLE_OPENCV
-from kartezio.model.evolution import KartezioES
-from kartezio.model.evolution import KartezioFitness, KartezioMutation
 from kartezio.model.helpers import Factory, Observer, Prototype
 from kartezio.model.helpers import Observable
 from kartezio.model.helpers import singleton
@@ -43,8 +48,126 @@ from kartezio.preprocessing import TransformToHED, TransformToHSV
 from kartezio.stacker import MeanKartezioStackerForWatershed
 from kartezio.stacker import StackerMean
 
+class KartezioMetric(KartezioNode, ABC):
+    def __init__(
+        self,
+        name: str,
+        symbol: str,
+        arity: int,
+    ):
+        super().__init__(name, symbol, arity, 0)
+
+    def _to_json_kwargs(self) -> dict:
+        pass
+
+class KartezioFitness(KartezioNode, ABC):
+    def __init__(
+        self,
+        name: str,
+        symbol: str,
+        arity: int,
+        default_metric: KartezioMetric = None,
+    ):
+        super().__init__(name, symbol, arity, 0)
+        self.metrics: MetricList = []
+        if default_metric:
+            self.add_metric(default_metric)
+
+    def add_metric(self, metric: KartezioMetric):
+        self.metrics.append(metric)
+
+    def call(self, y_true, y_pred) -> ScoreList:
+        scores: ScoreList = []
+        for yi_pred in y_pred:
+            scores.append(self.compute_one(y_true, yi_pred))
+        return scores
+
+    def compute_one(self, y_true, y_pred) -> Score:
+        score = 0.0
+        y_size = len(y_true)
+        for i in range(y_size):
+            _y_true = y_true[i].copy()
+            _y_pred = y_pred[i]
+            score += self.__fitness_sum(_y_true, _y_pred)
+        return Score(score / y_size)
+
+    def __fitness_sum(self, y_true, y_pred) -> Score:
+        score = Score(0.0)
+        for metric in self.metrics:
+            score += metric.call(y_true, y_pred)
+        return score
+
+    def _to_json_kwargs(self) -> dict:
+        pass
+
+
+class KartezioMutation(GenomeReaderWriter, ABC):
+    def __init__(self, shape, n_functions):
+        super().__init__(shape)
+        self.n_functions = n_functions
+        self.parameter_max_value = 256
+
+    def dumps(self) -> dict:
+        return {}
+
+    @property
+    def random_parameters(self):
+        return np.random.randint(self.parameter_max_value, size=self.shape.parameters)
+
+    @property
+    def random_functions(self):
+        return np.random.randint(self.n_functions)
+
+    @property
+    def random_output(self):
+        return np.random.randint(self.shape.out_idx, size=1)
+
+    def random_connections(self, idx: int):
+        return np.random.randint(
+            self.shape.nodes_idx + idx, size=self.shape.connections
+        )
+
+    def mutate_function(self, genome: KartezioGenome, idx: int):
+        self.write_function(genome, idx, self.random_functions)
+
+    def mutate_connections(
+        self, genome: KartezioGenome, idx: int, only_one: int = None
+    ):
+        new_connections = self.random_connections(idx)
+        if only_one is not None:
+            new_value = new_connections[only_one]
+            new_connections = self.read_connections(genome, idx)
+            new_connections[only_one] = new_value
+        self.write_connections(genome, idx, new_connections)
+
+    def mutate_parameters(self, genome: KartezioGenome, idx: int, only_one: int = None):
+        new_parameters = self.random_parameters
+        if only_one is not None:
+            old_parameters = self.read_parameters(genome, idx)
+            old_parameters[only_one] = new_parameters[only_one]
+            new_parameters = old_parameters.copy()
+        self.write_parameters(genome, idx, new_parameters)
+
+    def mutate_output(self, genome: KartezioGenome, idx: int):
+        self.write_output_connection(genome, idx, self.random_output)
+
+    @abstractmethod
+    def mutate(self, genome: KartezioGenome):
+        pass
+
+class KartezioES(ABC):
+    @abstractmethod
+    def selection(self):
+        pass
+
+    @abstractmethod
+    def reproduction(self):
+        pass
+
+
 class KartezioComponent(Serializable, ABC):
     pass
+
 
 class KartezioNode(KartezioComponent, ABC):
     """
@@ -981,13 +1104,6 @@ class ModelBuilder:
                 raise ValueError(
                     f"Stacker [{parser.stacker.name}] requires {parser.stacker.arity} output nodes. ({parser.shape.outputs} given)"
                 )
-
-        if not isinstance(fitness, KartezioFitness):
-            raise ValueError(f"Fitness {fitness} has not been properly set.")
-
-        if not isinstance(mutation_method, KartezioMutation):
-            raise ValueError(
-                f"Mutation {mutation_method} has not been properly set.")
 
         if dataset_inputs and (dataset_inputs != parser.shape.inputs):
             raise ValueError(
