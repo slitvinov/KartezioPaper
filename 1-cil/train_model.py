ceb8ada9c912948cdf9282fa99a18a4feded7ebd
Sergey Litvinov <slitvinov@gmail.com>
2024-09-20 15:20:53 +0200

    rm __main_

--- 1-cil/train_model.py
+++ 1-cil/train_model.py
@@ -11,21 +11,20 @@ CHANNELS = [1, 2]
 preprocessing = SelectChannels(CHANNELS)
 
 
-if __name__ == "__main__":
-    generations = 100
-    _lambda = 5
-    frequency = 5
-    indices = None  # [12, 26, 76, 59, 58, 37, 11, 79, 34, 35, 36, 81, 67, 17, 13]
-    model = create_instance_segmentation_model(
-        generations,
-        _lambda,
-        inputs=2,
-    )
-    dataset = read_dataset(DATASET, indices=indices)
-    elite, _ = train_model(
-        model,
-        dataset,
-        MODELS,
-        preprocessing=preprocessing,
-        callback_frequency=frequency,
-    )
+generations = 100
+_lambda = 5
+frequency = 5
+indices = None  # [12, 26, 76, 59, 58, 37, 11, 79, 34, 35, 36, 81, 67, 17, 13]
+model = create_instance_segmentation_model(
+    generations,
+    _lambda,
+    inputs=2,
+)
+dataset = read_dataset(DATASET, indices=indices)
+elite, _ = train_model(
+    model,
+    dataset,
+    MODELS,
+    preprocessing=preprocessing,
+    callback_frequency=frequency,
+)
