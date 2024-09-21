a6958248801599abd13a846766b3745b78d954df
Sergey Litvinov <slitvinov@gmail.com>
2024-09-21 09:28:29 +0200

    up

--- 2-melanoma_nuclei/train1.py
+++ 2-melanoma_nuclei/train1.py
@@ -151,11 +151,178 @@ registry = Registry()
 from kartezio.image.bundle import BUNDLE_OPENCV
 from kartezio.model.builder import ModelBuilder
 from kartezio.callback import CallbackSave, CallbackVerbose
-from kartezio.dataset import read_dataset
 from kartezio.enums import CSV_DATASET
-from kartezio.model.base import ModelCGP
-from kartezio.model.helpers import singleton
-from kartezio.utils.io import pack_one_directory
+from kartezio.model.components import KartezioGenome, KartezioParser
+
+def pack_one_directory(directory_path):
+    directory = Directory(directory_path)
+    packed_history = {}
+    elite = json_read(filepath=f"{directory_path}/elite.json")
+    packed_history["dataset"] = elite["dataset"]
+    packed_history["decoding"] = elite["decoding"]
+    packed_history["elite"] = elite["individual"]
+    packed_history["generations"] = []
+    generations = []
+    for g in directory.ls(f"G*.json", ordered=True):
+        generations.append(int(g.name.replace("G", "").split(".")[0]))
+    generations.sort()
+    for generation in generations:
+        current_generation = json_read(filepath=f"{directory_path}/G{generation}.json")
+        generation_json = {
+            "generation": generation,
+            "population": current_generation["population"],
+        }
+        packed_history["generations"].append(generation_json)
+    json_write(
+        filepath=f"{directory_path}/history.json", json_data=packed_history, indent=None
+    )
+    print(f"All generations packed in {directory_path}.")
+    for generation in generations:
+        file_to_delete = f"{directory_path}/G{generation}.json"
+        os.remove(file_to_delete)
+    print(f"All {len(generations)} generation files deleted.")
+
+
+class JsonLoader:
+    def read_individual(self, filepath):
+        json_data = json_read(filepath=filepath)
+        dataset = json_data["dataset"]
+        parser = KartezioParser.from_json(json_data["decoding"])
+        try:
+            individual = KartezioGenome.from_json(json_data["individual"])
+        except KeyError:
+            try:
+                individual = KartezioGenome.from_json(json_data)
+            except KeyError:
+                individual = KartezioGenome.from_json(json_data["population"][0])
+        return dataset, individual, parser
+
+
+class JsonSaver:
+    def __init__(self, dataset, parser):
+        self.dataset_json = json.from_dataset(dataset)
+        self.parser_as_json = parser.dumps()
+
+    def save_population(self, filepath, population):
+        json_data = {
+            "dataset": self.dataset_json,
+            "population": json.from_population(population),
+            "decoding": self.parser_as_json,
+        }
+        json_write(filepath, json_data)
+
+    def save_individual(self, filepath, individual):
+        json_data = {
+            "dataset": self.dataset_json,
+            "individual": json.from_individual(individual),
+            "decoding": self.parser_as_json,
+        }
+        json_write(filepath, json_data)
+
+
+class ModelML(ABC):
+    @abstractmethod
+    def fit(self, x: List, y: List):
+        pass
+
+    @abstractmethod
+    def evaluate(self, x: List, y: List):
+        pass
+
+    @abstractmethod
+    def predict(self, x: List):
+        pass
+
+
+class ModelGA(ABC):
+    def __init__(self, strategy, generations):
+        self.strategy = strategy
+        self.current_generation = 0
+        self.generations = generations
+
+    def fit(self, x: List, y: List):
+        pass
+
+    def initialization(self):
+        self.strategy.initialization()
+
+    def is_satisfying(self):
+        end_of_generations = self.current_generation >= self.generations
+        best_fitness_reached = self.strategy.population.fitness[0] == 0.0
+        return end_of_generations or best_fitness_reached
+
+    def selection(self):
+        self.strategy.selection()
+
+    def reproduction(self):
+        self.strategy.reproduction()
+
+    def mutation(self):
+        self.strategy.mutation()
+
+    def evaluation(self, y_true, y_pred):
+        self.strategy.evaluation(y_true, y_pred)
+
+    def next(self):
+        self.current_generation += 1
+
+
+class ModelCGP(ModelML, Observable):
+    def __init__(self, generations, strategy, parser):
+        super().__init__()
+        self.generations = generations
+        self.strategy = strategy
+        self.parser = parser
+        self.callbacks = []
+
+    def fit(
+        self,
+        x,
+        y,
+    ):
+        genetic_algorithm = ModelGA(self.strategy, self.generations)
+        genetic_algorithm.initialization()
+        y_pred = self.parser.parse_population(self.strategy.population, x)
+        genetic_algorithm.evaluation(y, y_pred)
+        self._notify(0, Event.START_LOOP, force=True)
+        while not genetic_algorithm.is_satisfying():
+            self._notify(genetic_algorithm.current_generation, Event.START_STEP)
+            genetic_algorithm.selection()
+            genetic_algorithm.reproduction()
+            genetic_algorithm.mutation()
+            y_pred = self.parser.parse_population(self.strategy.population, x)
+            genetic_algorithm.evaluation(y, y_pred)
+            genetic_algorithm.next()
+            self._notify(genetic_algorithm.current_generation, Event.END_STEP)
+        self._notify(genetic_algorithm.current_generation, Event.END_LOOP, force=True)
+        history = self.strategy.population.history()
+        elite = self.strategy.elite
+        return elite, history
+
+    def _notify(self, n, name, force=False):
+        event = {
+            "n": n,
+            "name": name,
+            "content": self.strategy.population.history(),
+            "force": force,
+        }
+        self.notify(event)
+
+    def evaluate(self, x, y):
+        y_pred, t = self.predict(x)
+        return self.strategy.fitness.compute(y, [y_pred])
+
+    def predict(self, x):
+        return self.parser.parse(self.strategy.elite, x)
+
+    def save_elite(self, filepath, dataset):
+        JsonSaver(dataset, self.parser).save_individual(
+            filepath, self.strategy.population.history().individuals[0]
+        )
+
+    def print_python_class(self, class_name):
+        python_writer = GenomeToPython(self.parser)
+        python_writer.to_python_class(class_name, self.strategy.elite)
 
 @singleton
 class TrainingArgs:
