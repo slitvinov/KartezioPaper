d1be3d83be93d798dd4fe37473117909a34a322c
Sergey Litvinov <slitvinov@gmail.com>
2024-09-23 11:54:42 +0200

    up

--- 2-melanoma_nuclei/train0.py
+++ 2-melanoma_nuclei/train0.py
@@ -44,9 +44,7 @@ from scipy.stats import kurtosis, skew
 from skimage.morphology import remove_small_holes, remove_small_objects
 from typing import List, NewType
 
-from kartezio.callback import CallbackSave
-from kartezio.callback import CallbackVerbose
-from kartezio.callback import Event
+from kartezio.model.components import KartezioCallback
 from kartezio.endpoint import EndpointWatershed
 from kartezio.enums import CSV_DATASET
 from kartezio.enums import DIR_PREVIEW
@@ -73,6 +71,49 @@ from kartezio.stacker import MeanKartezioStackerForWatershed
 from kartezio.stacker import StackerMean
 from kartezio.utils.io import JsonSaver
 from kartezio.utils.io import pack_one_directory
+
+class Event(Enum):
+    START_STEP = "on_step_start"
+    END_STEP = "on_step_end"
+    START_LOOP = "on_loop_start"
+    END_LOOP = "on_loop_end"
+
+
+class CallbackVerbose(KartezioCallback):
+    def _callback(self, n, e_name, e_content):
+        fitness, time = e_content.get_best_fitness()
+        if e_name == Event.END_STEP:
+            verbose = f"[G {n:04}] {fitness:.16f}"
+            print(verbose)
+        elif e_name == Event.END_LOOP:
+            verbose = f"[G {n:04}] {fitness:.16f}, loop done."
+            print(verbose)
+
+
+class CallbackSave(KartezioCallback):
+    def __init__(self, workdir, dataset, frequency=1):
+        super().__init__(frequency)
+        self.workdir = Directory(workdir).next(eventid())
+        self.dataset = dataset
+        self.json_saver = None
+
+    def set_parser(self, parser):
+        super().set_parser(parser)
+        self.json_saver = JsonSaver(self.dataset, self.parser)
+
+    def save_population(self, population, n):
+        filename = f"G{n}.json"
+        filepath = self.workdir / filename
+        self.json_saver.save_population(filepath, population)
+
+    def save_elite(self, elite):
+        filepath = self.workdir / JSON_ELITE
+        self.json_saver.save_individual(filepath, elite)
+
+    def _callback(self, n, e_name, e_content):
+        if e_name == Event.END_STEP or e_name == Event.END_LOOP:
+            self.save_population(e_content.get_individuals(), n)
+            self.save_elite(e_content.individuals[0])
 
 def register_fitness():
     """Force decorators to wrap KartezioFitness"""
