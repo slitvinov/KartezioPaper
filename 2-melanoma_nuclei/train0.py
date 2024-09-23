1040ad56f0cbc8f5cf3bd9a321f4912969d65a25
Sergey Litvinov <slitvinov@gmail.com>
2024-09-23 14:09:08 +0200

    rm unused readers

--- 2-melanoma_nuclei/train0.py
+++ 2-melanoma_nuclei/train0.py
@@ -2569,43 +2569,6 @@ class DataItem:
     def size(self):
         return len(self.datalist)
 
-
-@registry.readers.add("image_mask")
-class ImageMaskReader(DataReader):
-    def _read(self, filepath, shape=None):
-        if filepath == "":
-            mask = image_new(shape)
-            return DataItem([mask], shape, 0)
-        image = imread_grayscale(filepath)
-        _, labels = cv2.connectedComponents(image)
-        return DataItem([labels], image.shape[:2], len(np.unique(labels)) - 1, image)
-
-
-@registry.readers.add("image_hsv")
-class ImageHSVReader(DataReader):
-    def _read(self, filepath, shape=None):
-        image_bgr = imread_color(filepath)
-        image_hsv = bgr2hsv(image_bgr)
-        return DataItem(image_split(image_hsv), image_bgr.shape[:2], None, image_bgr)
-
-
-@registry.readers.add("image_hed")
-class ImageHEDReader(DataReader):
-    def _read(self, filepath, shape=None):
-        image_bgr = imread_color(filepath)
-        image_hed = bgr2hed(image_bgr)
-        return DataItem(image_split(image_hed), image_bgr.shape[:2], None, image_bgr)
-
-
-@registry.readers.add("image_labels")
-class ImageLabels(DataReader):
-    def _read(self, filepath, shape=None):
-        image = cv2.imread(filepath, cv2.IMREAD_ANYDEPTH)
-        for i, current_value in enumerate(np.unique(image)):
-            image[image == current_value] = i
-        return DataItem([image], image.shape[:2], image.max(), visual=image)
-
-
 @registry.readers.add("image_rgb")
 class ImageRGBReader(DataReader):
     def _read(self, filepath, shape=None):
@@ -2613,26 +2576,6 @@ class ImageRGBReader(DataReader):
         return DataItem(
             image_split(image), image.shape[:2], None, visual=rgb2bgr(image)
         )
-
-
-@registry.readers.add("csv_ellipse")
-class CsvEllipseReader(DataReader):
-    def _read(self, filepath, shape=None):
-        dataframe = pd.read_csv(filepath)
-        ellipses = read_ellipses_from_csv(
-            dataframe, scale=self.scale, ellipse_scale=1.0
-        )
-        label_mask = image_new(shape)
-        fill_ellipses_as_labels(label_mask, ellipses)
-        return DataItem([label_mask], shape, len(ellipses))
-
-
-@registry.readers.add("image_grayscale")
-class ImageGrayscaleReader(DataReader):
-    def _read(self, filepath, shape=None):
-        image = imread_grayscale(filepath)
-        visual = cv2.merge((image, image, image))
-        return DataItem([image], image.shape, None, visual=visual)
 
 
 @registry.readers.add("roi_polygon")
@@ -2644,43 +2587,6 @@ class RoiPolygonReader(DataReader):
         polygons = read_polygons_from_roi(filepath)
         fill_polygons_as_labels(label_mask, polygons)
         return DataItem([label_mask], shape, len(polygons))
-
-
-@registry.readers.add("one-hot_vector")
-class OneHotVectorReader(DataReader):
-    def _read(self, filepath, shape=None):
-        label = np.array(ast.literal_eval(filepath.split("/")[-1]))
-        return DataItem([label], shape, None)
-
-
-@registry.readers.add("image_channels")
-class ImageChannelsReader(DataReader):
-    def _read(self, filepath, shape=None):
-        image = imread_tiff(filepath)
-        if image.dtype == np.uint16:
-            raise ValueError(f"Image must be 8bits! ({filepath})")
-        shape = image.shape[-2:]
-        if len(image.shape) == 2:
-            channels = [image]
-            preview = gray2rgb(channels[0])
-        if len(image.shape) == 3:
-            # channels: (c, h, w)
-            channels = [channel for channel in image]
-            preview = cv2.merge(
-                (image_new(channels[0].shape), channels[0], channels[1])
-            )
-        if len(image.shape) == 4:
-            # stack: (z, c, h, w)
-            channels = [image[:, i] for i in range(len(image[0]))]
-            preview = cv2.merge(
-                (
-                    channels[0].max(axis=0).astype(np.uint8),
-                    channels[1].max(axis=0).astype(np.uint8),
-                    image_new(channels[0][0].shape, dtype=np.uint8),
-                )
-            )
-            cv2.imwrite("rgb_image.png", preview)
-        return DataItem(channels, shape, None, visual=preview)
 
 
 @dataclass
