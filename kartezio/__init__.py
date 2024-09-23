63cac33e4b4de24a8102c081dc288a3689c905f6
Sergey Litvinov <slitvinov@gmail.com>
2024-09-23 11:49:47 +0200

    dont import

--- kartezio/__init__.py
+++ kartezio/__init__.py
@@ -2,9 +2,3 @@
 
 """
 
-from kartezio.endpoint import register_endpoints
-from kartezio.fitness import register_fitness
-from kartezio.image.nodes import register_nodes
-from kartezio.metric import register_metrics
-from kartezio.stacker import register_stackers
-
