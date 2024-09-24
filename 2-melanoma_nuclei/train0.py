9bcf667dfd5e4d05abb2a45ae084409248b7f695
Sergey Litvinov <slitvinov@gmail.com>
2024-09-24 10:46:02 +0200

    KartezioMutation

--- 2-melanoma_nuclei/train0.py
+++ 2-melanoma_nuclei/train0.py
@@ -558,46 +558,6 @@ class KartezioMutation:
     def mutate_output(self, genome, idx):
         self.write_output_connection(genome, idx, self.random_output)
 
-
-class KartezioPopulation:
-
-    def __init__(self):
-        self.individuals = [None] * (g._lambda + 1)
-        self._fitness = {
-            "fitness": np.zeros(g._lambda + 1),
-            "time": np.zeros(g._lambda + 1)
-        }
-
-    def get_best_individual(self):
-        pass
-
-    def __getitem__(self, item):
-        return self.individuals.__getitem__(item)
-
-    def __setitem__(self, key, value):
-        self.individuals.__setitem__(key, value)
-
-    def set_time(self, individual, value):
-        self._fitness["time"][individual] = value
-
-    def set_fitness(self, fitness):
-        self._fitness["fitness"] = fitness
-
-    @property
-    def fitness(self):
-        return self._fitness["fitness"]
-
-    @property
-    def time(self):
-        return self._fitness["time"]
-
-    @property
-    def score(self):
-        score_list = list(zip(self.fitness, self.time))
-        return np.array(score_list,
-                        dtype=[("fitness", float), ("time", float)])
-
-
 @jit(nopython=True)
 def _label_overlap(x, y):
     x = x.ravel()
@@ -1721,10 +1681,40 @@ class PopulationHistory:
         return self.individuals.items()
 
 
-class PopulationWithElite(KartezioPopulation):
-
+class PopulationWithElite:
     def __init__(self):
-        super().__init__()
+        self.individuals = [None] * (g._lambda + 1)
+        self._fitness = {
+            "fitness": np.zeros(g._lambda + 1),
+            "time": np.zeros(g._lambda + 1)
+        }
+
+    def __getitem__(self, item):
+        return self.individuals.__getitem__(item)
+
+    def __setitem__(self, key, value):
+        self.individuals.__setitem__(key, value)
+
+    def set_time(self, individual, value):
+        self._fitness["time"][individual] = value
+
+    def set_fitness(self, fitness):
+        self._fitness["fitness"] = fitness
+
+    @property
+    def fitness(self):
+        return self._fitness["fitness"]
+
+    @property
+    def time(self):
+        return self._fitness["time"]
+
+    @property
+    def score(self):
+        score_list = list(zip(self.fitness, self.time))
+        return np.array(score_list,
+                        dtype=[("fitness", float), ("time", float)])
+
 
     def set_elite(self, individual):
         self[0] = individual
