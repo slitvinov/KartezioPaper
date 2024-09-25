6aa9bf4cc661e9648357a4dd394516de477b84a5
Sergey Litvinov <slitvinov@gmail.com>
2024-09-25 21:11:46 +0200

    use mutate_connections0 and mutate_connections1

--- main.py
+++ main.py
@@ -126,8 +126,14 @@ def true_positive0(iou):
     match_ok = iou[true_ind, pred_ind] >= th
     return match_ok.sum()
 
+def mutate_connections0(genome, idx):
+    new_connections = np.random.randint(g.inputs + idx, size=g.arity)
+    new_value = new_connections[None]
+    new_connections = genome[g.inputs + idx, 1:g.para_idx]
+    new_connections[None] = new_value
+    genome[g.inputs + idx, 1:g.para_idx] = new_connections
 
-def mutate_connections(genome, idx, only_one):
+def mutate_connections1(genome, idx, only_one):
     new_connections = np.random.randint(g.inputs + idx, size=g.arity)
     new_value = new_connections[only_one]
     new_connections = genome[g.inputs + idx, 1:g.para_idx]
@@ -153,7 +159,7 @@ def mutate1(genome):
             genome[g.inputs + idx, 0] = np.random.randint(len(g.nodes))
         elif mutation_parameter_index <= g.arity:
             connection_idx = mutation_parameter_index - 1
-            mutate_connections(genome, idx, only_one=connection_idx)
+            mutate_connections1(genome, idx, only_one=connection_idx)
         else:
             parameter_idx = mutation_parameter_index - g.arity - 1
             mutate_parameters1(genome, idx, only_one=parameter_idx)
@@ -205,7 +211,7 @@ for row in dataframe.itertuples():
 for genome in g.individuals:
     for j in range(g.n):
         genome[g.inputs + j, 0] = random.randrange(len(g.nodes))
-        mutate_connections(genome, j, None)
+        mutate_connections0(genome, j)
         new_parameters = np.random.randint(g.max_val, size=g.parameters)
         genome[g.inputs + j, g.para_idx:] = new_parameters
     for j in range(g.outputs):
