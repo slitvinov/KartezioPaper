9707538bc9a371d6bc41f8e69ed5fe9acece1f5b
Sergey Litvinov <slitvinov@gmail.com>
2024-11-11 14:03:46 +0100

    add wave.py

---
+++ wave.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+
+def u0(x):
+    a = abs(x)
+    return 1 - a if a <= 1 else 0
+
+
+def u1(t, x):
+    return u0(t - x)
+
+
+tend = 1.6
+lmb = 0.8
+M = 50
+lo, hi = -2, 3
+h = (hi - lo) / M
+k = lmb * h
+x = [lo + i * h for i in range(M + 1)]
+t = 0
+v = [u0(x) for x in x]
+u = [None] * (M + 1)
+while t < tend:
+    t += k
+    u[0] = 0
+    for m in range(1, M):
+        u[m] = (v[m - 1] + v[m + 1]) / 2 - lmb * (v[m + 1] - v[m - 1]) / 2
+    u[M] = u[M - 1]
+    u, v = v, u
+plt.plot(x, v, 'ko-')
+plt.plot(x, [u1(t, x) for x in x], '-k')
+plt.show()
